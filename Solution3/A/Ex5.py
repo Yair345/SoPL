483af6becfaefe8e7025df4c41ac196ce9b79bfc
Yair345 <yairlasri3@gmail.com>
2024-09-27 00:24:14 +0300

    Fixed A.Ex5 in Solution3

--- Solution3/A/Ex5.py
+++ Solution3/A/Ex5.py
@@ -1,11 +1,27 @@
 def sortedzip(lists):
-    if not lists:
+    if not all(lists):
         return []
-    sorted_first = sorted(lists[0])
-    rest_sorted_zip = sortedzip(lists[1:])
 
-    return zip(sorted_first, *rest_sorted_zip)
+    min_vals = [min(l) for l in lists]
+    # print(min_vals)
+
+    return [tuple(min_vals)] + sortedzip([[x for x in l if x != min_vals[i]] for i, l in enumerate(lists)])
 
 
 result = sortedzip([[3, 1, 2], [5, 6, 4], ['a', 'b', 'c']])
+print(list(result))
+
+
+def sortedzip_tail(lists):
+    def sortedzip_tail_helper(lists, result):
+        if not all(lists):
+            return result
+
+        min_vals = [min(l) for l in lists]
+
+        return sortedzip_tail_helper([[x for x in l if x != min_vals[i]] for i, l in enumerate(lists)], result + [tuple(min_vals)])
+
+    return sortedzip_tail_helper(lists, [])
+
+result = sortedzip_tail([[3, 1, 2], [5, 6, 4], ['a', 'b', 'c']])
 print(list(result))
